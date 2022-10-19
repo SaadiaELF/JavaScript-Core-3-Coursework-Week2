4fb3c9ab9f7bcc9dc5cfd2e691d3b188ef7bbabb
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-19 12:38:32 +0100

    Added handle error function

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -3,17 +3,27 @@ let addBtn = document.querySelector(".add-btn");
 let deleteBtn = document.querySelector(".delete-btn");
 let url = "https://dog.ceo/api/breeds/image/random";
 
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
+
 function getImage() {
   let itemElt = document.createElement("li");
   let imgElt = document.createElement("img");
   itemElt.className = "image-item";
   imgElt.className = "image";
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => handleErrors(response))
     .then((data) => {
       imgElt.src = data.message;
       itemElt.append(imgElt);
       imagesContainer.append(itemElt);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 }
 
